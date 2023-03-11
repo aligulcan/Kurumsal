32357849cd77f5f1aeeb7c6155453fbaaa3b9562
aligulcannn <aligulcan99@gmail.com>
2023-03-11 03:28:18 +0300

    Temel Güncelleme
    Temel Bİleşenler Eklendi

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PageController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\UserController;
+
+use App\Http\Controllers\FrontEndController;
+
+//Auth::routes();
+
+
+//Admin Yönlendirmeleri
+
+
+
+//Site Yönlendirmeleri
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
